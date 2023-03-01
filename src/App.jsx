e0b2c14f667428cc6913bc9d00a319bb75a20655
Sávio Haistém <savio.dantas.5858@gmail.com>
2023-03-01 20:10:29 -0300

    Adicionando estrutura inicial

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './css/App.css'
+import SearchBar from './components/SearchBar'
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar/>      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,15 @@
+import '../css/searchbar.css'
+
+function SearchBar() {
+    return (
+    <>
+        <div className="SearchArea">
+            <div className="SearchBar">
+                <p> search </p>
+            </div>    
+        </div>    
+    </>
+    );
+}
+
+export default SearchBar;
