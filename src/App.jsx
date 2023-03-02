2321e11ae3684de2acd33ac09a6d9997dc1e4ba7
Sávio Haistém <savio.dantas.5858@gmail.com>
2023-03-02 16:48:54 -0300

    Completando filtro e adicionando FontWesome

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './css/global.css'
 import SearchBar from './components/SearchBar'
 import SearchArea from './components/SearchArea'
+import SearchFilter from './components/SearchFilter'
 
 function App() {
   return (
     <>
       <SearchArea>
         <SearchBar/>
+        <SearchFilter/>
       </SearchArea>      
     </>
   )
--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -1,8 +1,13 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
+library.add(faFilter);
+import '../css/searchfilter.css'
+
 function SearchFilter() {
   return (
     <>
-      <div className="searchfilter">
-        
+      <div className="searchfilter transparentEffect">
+        <i className="filtericon fas fa-filter"></i>
       </div>
     </>
   );
