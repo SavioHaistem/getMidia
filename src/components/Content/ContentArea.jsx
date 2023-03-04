b02c4633513927888db67eade1ab972996b0f295
Sávio Haistém <savio.dantas.5858@gmail.com>
2023-03-04 10:18:14 -0300

    List Items versão inicial

--- src/components/Content/ContentArea.jsx
+++ src/components/Content/ContentArea.jsx
@@ -1,10 +1,19 @@
 import '../../css/contentarea.css'
+import data from '../../data/base';
 
 function ContentArea(props) {
+  const basedata = new data;
   return (
     <>
       <div className="contentarea">
-        {props.children}
+        <ol className='contentlist'>
+          {
+            basedata.data.map(item => 
+              <li className='contentitem  transparentEffect'>
+                <h3> { item.item } </h3>
+              </li>
+          )}
+        </ol>
       </div>
     </>
   );
---
+++ src/data/base.js
@@ -0,0 +1,39 @@
+class data {
+  constructor() {
+    this.data = 
+    [
+      {item: "lapada"},
+      {item: "corda"},
+      {item: "mão"},
+      {item: "maçã"},
+      {item: "ovo"},
+      {item: "abacate"},
+      {item: "lapada"},
+      {item: "corda"},
+      {item: "mão"},
+      {item: "maçã"},
+      {item: "ovo"},
+      {item: "lapada"},
+      {item: "corda"},
+      {item: "mão"},
+      {item: "maçã"},
+      {item: "ovo"},
+      {item: "lapada"},
+      {item: "corda"},
+      {item: "mão"},
+      {item: "maçã"},
+      {item: "ovo"},
+      {item: "lapada"},
+      {item: "corda"},
+      {item: "mão"},
+      {item: "maçã"},
+      {item: "ovo"},  
+    ]
+  }
+  set(item) {
+    let Item = {item: item}
+    this.data.push(item)
+  }
+}
+
+export default data;
