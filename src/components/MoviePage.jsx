4747e2e0f6d9109a34a3bd70d0ec51162159ead3
Sávio Haistém <savio.dantas.5858@gmail.com>
2023-03-31 12:24:02 -0300

    Estilizando aba de filmes

--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import GetData from './GetData';
+import '../css/MoviePage.css'
+
 
 function MoviePage({data}) {
-  console.log(data && data.original_title)
+
+  console.dir(data && data)
   return(
-    <div>
-      
+  <>
+    <div className='movieTab transparentEffect'>
+      <div className="backgroundMovieTab">
+        <img src={`https://image.tmdb.org/t/p/original/${data?.backdrop_path}`} alt="" />
+      </div>
     </div>
+  </>
   )
 }
 
