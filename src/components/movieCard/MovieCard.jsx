2b4743e04ae83dc38975cf817a7a5b2720fc2878
Sávio Haistém <savio.dantas.5858@gmail.com>
2023-04-21 17:08:57 -0300

    Corrigindo imagens não carregadas do Card

--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,16 +1,17 @@
 import './css/movieCard.css'
+import poster from '../../../public/imgs/Card.png'
 
 function ContentMovieCard({movie}) {
   return (
   <>
     <div className="movieCard transparentEffect">
       <div className="movieCardBackground">
-        <img src={`${`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}`} alt={`${movie.name}`} />
+        <img src={movie.poster_path ? `${`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}` : poster } alt={`${movie.name}`} />
       </div>
         <div className='gradientBlackEffect'></div>
         <div className="posterWrapper">
           <p className="cardText">{movie.title||movie.name}</p>
-            <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+            <img src={ movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.poster_path}` : poster }
               alt={`${movie.name}_Background`}
               className="moviePoster" />
         </div>                 
