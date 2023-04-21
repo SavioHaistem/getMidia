428209d280815713912f238d1dde8079c7d3760a
Sávio Haistém <savio.dantas.5858@gmail.com>
2023-04-21 08:55:12 -0300

    Corrigindo Redefinição de páginas automaticamente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './css/global.css';
 import Home from './routes/home/components/Home';
 import MoviePage from './routes/movie/components/MoviePage';
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 
 export const AppContext = createContext(null)
@@ -12,6 +12,8 @@ export function GetMidia() {
   const [Page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
 
+  useEffect(()=>setPage(1),[userSearch])
+  
   return (  
     <>
       <AppContext.Provider value={{ userSearch, Page, setPage, setUserSearch, totalPages, setTotalPages }}>
